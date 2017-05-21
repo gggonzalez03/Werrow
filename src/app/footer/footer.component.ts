702c13f8d55c7a444f1a9ef4ca09867154d12be4
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-05-20 22:45:29 -0700

    style: Added footer
    Footer component is added so that the whole website
    can reuse the code and use it on pages.

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: [
+    './footer.component.css',
+    './Footer-Basic.css'
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
