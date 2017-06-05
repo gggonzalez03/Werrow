2c3e320b831bf3911c2f4796df4bcbf449a2265f
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-06-05 06:52:47 -0700

    feat: Added endpoint for fetching single post

--- werrow-server/routes/borrow-post-routes.js
+++ werrow-server/routes/borrow-post-routes.js
@@ -44,4 +44,23 @@ routes.get('/borrows', (req, res) => {
   })
 })
 
+routes.get('/:id', (req, res) => {
+  BorrowPost.find({_id: req.params.id})
+  .exec((err, borrow) => {
+    if (!err) {
+      res.status(200).json({
+        status: "200",
+        message: "OK",
+        data: borrow
+      })
+    }
+    else {
+      res.status(500).json({
+        status: "500",
+        message: "Internal Server Error." 
+     })
+    }
+  })
+})
+
 module.exports = routes;
