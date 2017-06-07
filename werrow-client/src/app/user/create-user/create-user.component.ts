9b36bdc10fc96da6676448c3955a73922795116f
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-06-06 22:56:46 -0700

    feat: Form validation on Sign Up page
    The Sign Up page now informs the user when they are typing invalid inputs or
    when they forget to put something on required textfields.

--- werrow-client/src/app/user/create-user/create-user.component.ts
+++ werrow-client/src/app/user/create-user/create-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { User } from '../user';
 import { Router } from '@angular/router';
 import { UserService } from '../user.service'
@@ -15,35 +15,39 @@ export class CreateUserComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-        name: new FormControl(),
-        email: new FormControl(),
-        password: new FormControl()
+      name: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
 
   // Creates user by asking UserService to add
   // user to the database
   createUser() {
-    var newUser = new User();
 
-    newUser.name = this.signUpForm.value.name;
-    newUser.email = this.signUpForm.value.email;
-    newUser.password = this.signUpForm.value.password;
+    if (!this.signUpForm.invalid) {
+      var newUser = new User();
 
-    // TODO: uncomment this when connected to the database
-    this.userService.createUser(newUser)
-    .then(status => {
-      console.log(status);
-      this.router.navigate(['/home']);
-    })
-    .catch(err => console.log(err));
+      newUser.name = this.signUpForm.value.name;
+      newUser.email = this.signUpForm.value.email;
+      newUser.password = this.signUpForm.value.password;
 
-    // Login the user after account creation
-    // this.loginUser(user);
+      // TODO: uncomment this when connected to the database
+      this.userService.createUser(newUser)
+        .then(status => {
+          console.log(status);
+          this.router.navigate(['/home']);
+        })
+        .catch(err => console.log(err));
+
+      // Login the user after account creation
+      // this.loginUser(user);
+    }
   }
 }
