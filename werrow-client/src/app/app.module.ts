d7571911635a1dd8838acc65a9f10b9a4b0a5b3d
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-06-08 21:20:07 -0700

    start: User profile page
    User profile page will be on the works for the next updates.

--- werrow-client/src/app/app.module.ts
+++ werrow-client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { UserService } from './user/user.service';
 import { BorrowRequestService } from './borrow-request/borrow-request.service';
 import { GoogleMapService } from './google-map/google-map.service';
 import { HeaderService } from './header/header.service';
+import { UserProfileService } from './user-profile/user-profile.service'
 import { BorrowRequestComponent } from './borrow-request/borrow-request.component';
 import { CreateUserComponent } from './user/create-user/create-user.component';
 import { CreatePostComponent } from './borrow-request/create-post/create-post.component';
@@ -18,6 +19,7 @@ import { LogoutUserComponent } from './user/logout-user/logout-user.component';
 import { GoogleMapComponent } from './google-map/google-map.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 @NgModule({
@@ -31,7 +33,8 @@ import { FooterComponent } from './footer/footer.component';
     LogoutUserComponent,
     GoogleMapComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -61,7 +64,8 @@ import { FooterComponent } from './footer/footer.component';
     UserService,
     BorrowRequestService,
     GoogleMapService,
-    HeaderService
+    HeaderService,
+    UserProfileService
   ],
   bootstrap: [AppComponent]
 })
